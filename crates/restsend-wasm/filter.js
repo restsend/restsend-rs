56a3269169eb981268f0f9c649aeef018481c0cd
jinti <shenjindi@fourz.cn>
2024-05-27 10:44:41 +0800

    fix filter.js typo issue

--- crates/restsend-wasm/filter.js
+++ crates/restsend-wasm/filter.js
@@ -1,25 +1,23 @@
 /*
     * filterConversations
     * Retrieve conversations from the indexedDB table that satisfy the predicate function and are not cancelled
-    * @param {Object} options
-    * @param {string} options.tblName - the name of the table
-    * @param {function} options.predicate - a function that takes a conversation and returns a boolean
-    * @param {function} options.isCancelled - a function that returns a boolean
-    * @param {number} options.updatedAt - the timestamp of the last conversation to retrieve
-    * @param {number} options.limit - the maximum number of conversations to retrieve
+    * @param {string} name - the name of the table
+    * @param {function} predicate - a function that takes a conversation and returns a boolean
+    * @param {function} isCancelled - a function that returns a boolean
+    * @param {number} updatedAt - the timestamp of the last conversation to retrieve
+    * @param {number} limit - the maximum number of conversations to retrieve
 */
-export async function filterConversations({
-    tblName: name,
+export async function filterConversations(name,
     predicate,
     isCancelled,
     updatedAt,
-    limit
-}) {
+    limit = 200
+) {
     const version = 1
     const request = window.indexedDB.open(name, version)
 
     return new Promise((resolve, reject) => {
-        result.onerror = (event) => {
+        request.onerror = (event) => {
             reject(event)
         }
         request.onsuccess = (event) => {
@@ -35,17 +33,17 @@ export async function filterConversations({
                 range = IDBKeyRange.bound(['', Infinity], ['', timestamp], false, true)
             }
 
-            const cursor = index.openCursor(range)
+            const cursor = index.openCursor(range, direction = 'prev')
             let items = []
             cursor.onsuccess = (event) => {
                 const cursor = event.target.result
-                if (!cursor || (isCancelled && isCancelled()) || (limit !== undefined && items.length >= limit)) {
+                if (!cursor || (isCancelled != undefined && isCancelled()) || (limit !== undefined && items.length >= limit)) {
                     db.close()
                     resolve(items)
                     return
                 }
                 const value = JSON.parse(cursor.value.value)
-                if (predicate && !predicate(value)) {
+                if (predicate != undefined && !predicate(value)) {
                     cursor.continue()
                     return
                 }
@@ -53,9 +51,9 @@ export async function filterConversations({
                 cursor.continue()
                 return
             }
+            cursor.onerror = (event) => {
+                reject(event)
+            };
         }
-        cursor.onerror = (event) => {
-            reject(event)
-        };
     });
 }
